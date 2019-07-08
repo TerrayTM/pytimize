c7f72c8cf7adc7b00f490af11c5e41ea2765d897
Terry Zheng <terryzhengtx@gmail.com>
2019-07-08 17:07:15 -0400

    Improve constructor tests

--- tests/init_test.py
+++ tests/init_test.py
@@ -2,9 +2,11 @@ import sys
 import numpy as np
 
 sys.path.append("../optimization")
+sys.path.append("../optimization/enums")
 
 from main import LinearProgrammingModel
 from unittest import TestCase, main
+from objective import Objective
 from math import isclose
 
 # To do: add test cases for optional parameters
@@ -85,7 +87,22 @@ class TestInit(TestCase):
         with self.assertRaises(ValueError, msg="Should throw exception if arrays are empty."):
             p = LinearProgrammingModel(A, b, c, z)
 
-    def test_invalid_types(self):
+        A = np.array([
+            [1, 2],
+            [5, 6],
+            [9, 10]
+        ])
+        b = np.array([6, 8, 10])
+        c = np.array([100, 300])
+        z = 5
+
+        with self.assertRaises(ValueError, msg="Should throw exception if dimension mismatch between inequalities amd A."):
+            p = LinearProgrammingModel(A, b, c, z, inequalities=[])
+
+        with self.assertRaises(ValueError, msg="Should throw exception if dimension mismatch between inequalities amd A."):
+            p = LinearProgrammingModel(A, b, c, z, inequalities=['=', '<=', '>=', '>='])
+
+    def test_invalid_values(self):
         A = np.array([
             ["a", 2, "b"],
             [5, 6, "c"],
@@ -94,9 +111,10 @@ class TestInit(TestCase):
         b = np.array([6, 8, 10])
         c = np.array([100, 200, 300])
         z = 5
+        objective = Objective.min
 
-        with self.assertRaises(TypeError, msg="Should throw exception type of A is incorrect."):
-            p = LinearProgrammingModel(A, b, c, z)
+        with self.assertRaises(ValueError, msg="Should throw exception if type of A is incorrect."):
+            p = LinearProgrammingModel(A, b, c, z, objective)
 
         A = np.array([
             [1, 2, 3],
@@ -105,20 +123,42 @@ class TestInit(TestCase):
         ])
         b = "test"
 
-        with self.assertRaises(TypeError, msg="Should throw exception type of b is incorrect."):
-            p = LinearProgrammingModel(A, b, c, z)
+        with self.assertRaises(ValueError, msg="Should throw exception if type of b is incorrect."):
+            p = LinearProgrammingModel(A, b, c, z, objective)
 
         b = np.array([6, 8, 10])
         c = 6
 
-        with self.assertRaises(TypeError, msg="Should throw exception type of c is incorrect."):
-            p = LinearProgrammingModel(A, b, c, z)
+        with self.assertRaises(ValueError, msg="Should throw exception if type of c is incorrect."):
+            p = LinearProgrammingModel(A, b, c, z, objective)
 
         c = np.array([100, 200, 300])
         z = "test"
 
-        with self.assertRaises(TypeError, msg="Should throw exception type of z is incorrect."):
-            p = LinearProgrammingModel(A, b, c, z)
+        with self.assertRaises(ValueError, msg="Should throw exception if type of z is incorrect."):
+            p = LinearProgrammingModel(A, b, c, z, objective)
+        
+        z = 10
+        objective = "min"
+
+        with self.assertRaises(ValueError, msg="Should throw exception if type of objective is incorrect."):
+            p = LinearProgrammingModel(A, b, c, z, objective)
+
+    def test_sef_detection(self):
+        p = LinearProgrammingModel([[1, 2, 3], [4, 5, 6]], [10, 20], [100, 300, 500], 20)
+
+        self.assertTrue(p.is_sef, "Should detect SEF form.")
+
+        p = LinearProgrammingModel([[1, 2, 3], [4, 5, 6]], [10, 20], [100, 300, 500], 20, inequalities=['=', '='])
+
+        self.assertTrue(p.is_sef, "Should detect SEF form.")
+
+        p = LinearProgrammingModel([[1, 2, 3], [4, 5, 6]], [10, 20], [100, 300, 500], 20, inequalities=['<=', '='])
+
+        self.assertFalse(p.is_sef, "Should detect non-SEF form")
+
+        # To do: add cases on free variables
+
 
 if __name__ == "__main__":
     main()
