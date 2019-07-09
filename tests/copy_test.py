b0972d1dfa2d08692038c4874bf3aae96fb22f0e
Terry Zheng <terryzhengtx@gmail.com>
2019-07-09 09:39:27 -0400

    Update copy test

--- tests/copy_test.py
+++ tests/copy_test.py
@@ -2,9 +2,11 @@ import sys
 import numpy as np
 
 sys.path.append("../optimization")
+sys.path.append("../optimization/enums")
 
 from main import LinearProgrammingModel
 from unittest import TestCase, main
+from objective import Objective
 from math import isclose
 
 class TestCopy(TestCase):
@@ -12,20 +14,27 @@ class TestCopy(TestCase):
         A = np.array([
             [1, 2, 3],
             [4, 5, 6],
-            [7, 8, 9]
+            [7, 8, 9],
+            [10, 11, 12],
+            [13, 14, 15],
+            [16, 17, 18],
+            [19, 20, 21],
+            [22, 23, 24]
         ])
-        b = np.array([6, 8, 10])
+        b = np.array([6, 8, 10, 12, 14, 16, 18, 20])
         c = np.array([100, 200, 300])
         z = 5
-        # Check for objective, free vars, and symbols
 
-        p = LinearProgrammingModel(A, b, c, z)
+        p = LinearProgrammingModel(A, b, c, z, Objective.min, ['=', '>=', '<=', '=', '>=', '<=', '=', '='])
         copy = p.copy()
 
         self.assertTrue(np.allclose(p.A, copy.A), "Should be the same coefficient matrix.")
         self.assertTrue(np.allclose(p.b, copy.b), "Should be the same constraints.")
         self.assertTrue(np.allclose(p.c, copy.c), "Should be the same coefficient vector.")
         self.assertTrue(isclose(p.z, copy.z), "Should be the same constant.")
+        self.assertTrue(copy.objective == Objective.min, "Should be the same objective.")
+        self.assertTrue(copy.inequalities == ['=', '>=', '<=', '=', '>=', '<=', '=', '='], "Should be the same inequalities.")
+        
 
 if __name__ == "__main__":
     main()
