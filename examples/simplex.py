b00aa2cd8e066227653996b0490b3766d78fa077
Terry Zheng <terryzhengtx@gmail.com>
2019-11-24 14:40:22 -0500

    Add simplex example

---
+++ examples/simplex.py
@@ -0,0 +1,22 @@
+import sys
+import numpy as np
+
+sys.path.append("../optimization")
+
+from main import LinearProgram
+
+A = np.array([
+    [1, 0, 2, 7, -1], 
+    [0, 1, -4, -5, 3]
+])
+b = np.array([2, 1])
+c = np.array([0, 0, 4, -11, -1])
+z = 17
+
+p = LinearProgram(A, b, c, z)
+optimal_solution, optimal_basis, optimality_certificate = p.simplex_solution([1, 2])
+
+print("Optimal Solution: {}".format(optimal_solution))
+print("Optimal Basis: {}".format(optimal_basis))
+print("Optimality Certificate: {}".format(optimality_certificate))
+
