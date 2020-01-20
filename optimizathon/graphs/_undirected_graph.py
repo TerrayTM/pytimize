aae135741b0325c60f505a83ec7d7a2dbb31b3ad
Terry Zheng (tezheng) <tezheng@cisco.com>
2020-01-20 10:29:27 -0500

    Improve graph

--- optimizathon/graphs/_undirected_graph.py
+++ optimizathon/graphs/_undirected_graph.py
@@ -1,10 +1,10 @@
 import math
 
-from typing import List, Tuple
-#TODO underscore graph
+from typing import List, Tuple, Dict, Set
+
 class UndirectedGraph: #TODO validation 
   def __init__(self, edges: List[Tuple[str, str, float]]=None) -> None:
-    self.graph = {}
+    self._graph = {}
 
     if edges is not None:
       if not isinstance(edges, list):
@@ -25,7 +25,7 @@ class UndirectedGraph: #TODO validation
 
 
   def add_edge(self, edge: Tuple[str, str], weight: float=0) -> None:
-    if edge[0] in self.graph.keys() and edge[1] in self.graph[edge[0]]:
+    if edge[0] in self._graph.keys() and edge[1] in self._graph[edge[0]]:
       raise ValueError("The given edge is already in graph.")
 
     if weight < 0:
@@ -34,8 +34,11 @@ class UndirectedGraph: #TODO validation
     if edge[0] == edge[1]:
       raise ValueError("Cannot form an edge with the same vertex.")
 
-    self.graph.setdefault(edge[0], set()).add((edge[1], weight))
-    self.graph.setdefault(edge[1], set()).add((edge[0], weight))
+    if not edge[0] or not edge[1]:
+      raise ValueError("Edge name cannot be empty string.") 
+
+    self._graph.setdefault(edge[0], set()).add((edge[1], weight))
+    self._graph.setdefault(edge[1], set()).add((edge[0], weight))
 
 
 
@@ -43,26 +46,34 @@ class UndirectedGraph: #TODO validation
     if not self.has_edge(edge):
       raise ValueError("The given edge is not in graph.")
 
-    self.graph[edge[0]].remove(edge[1])
-    self.graph[edge[1]].remove(edge[0])
+    self._graph[edge[0]].remove(edge[1])
+    self._graph[edge[1]].remove(edge[0])
 
 
 
   def has_edge(self, edge: Tuple[str, str]) -> bool:
-    return edge[0] in self.graph.keys() and edge[1] in self.graph[edge[0]]
+    return edge[0] in self._graph.keys() and edge[1] in self._graph[edge[0]]
 
 
 
   def has_vertex(self, vertex: str) -> bool:
-    return vertex in self.graph.keys()
+    return vertex in self._graph.keys()
+
+
+
+  def get_weight(self, edge: Tuple[str, str]) -> float:
+    if not self.has_edge(edge):
+      raise ValueError("The given edge is not in graph.")
+
+    return self._graph[edge[0]][edge][1]
 
 
 
   def shortest_path(self, start: str, end: str) -> List[Tuple[str, str]]:
-    if start not in self.graph.keys():
+    if start not in self._graph.keys():
       raise ValueError("Starting vertex is not in graph.")
 
-    if end not in self.graph.keys():
+    if end not in self._graph.keys():
       raise ValueError("Ending vertex is not in graph.")
 
     potential = {}
@@ -75,7 +86,7 @@ class UndirectedGraph: #TODO validation
       edges = set()
 
       for vertex in visited:
-        for edge in self.graph[vertex]:
+        for edge in self._graph[vertex]:
           if edge[0] not in visited:
             edge_hash = [edge[0], vertex]
 
@@ -114,7 +125,38 @@ class UndirectedGraph: #TODO validation
 
 
 
-  def to_linear_program(self):
+  def create_shortest_path_program(self, start: str, end: str) -> "LinearProgram":
+    if start not in self._graph.keys():
+      raise ValueError("Starting vertex is not in graph.")
+
+    if end not in self._graph.keys():
+      raise ValueError("Ending vertex is not in graph.")
+
+    vertices = self.vertices
+
+    vertices.remove(start)
+    vertices.remove(end)
+
+    power_set = set()
+
+    for i in 2 ** range(len(vertices)):
+      current_set = set()
+
+      for j in range(len(vertices)):
+        if i & (1 << j):
+          current_set.add(vertices[j])
+
+      current_set.add(start)
+      power_set.add(current_set)
+    
+    for group in power_set:
+      edges = set()
+
+      for edge in group:
+        edges = edges.union(self._graph[edge])
+
+      edges = filter(lambda x: x[0] not in group, edges) # [(a,0)]
+
     pass
 
 
@@ -122,41 +164,50 @@ class UndirectedGraph: #TODO validation
   def __get_edges(self) -> List[Tuple[str, str]]:
     result = set()
 
-    for vertex, connections in self.graph:
+    for vertex, connections in self._graph:
       for connection in connections:
-        result.add((vertex, connection))
+        result.add((vertex, connection[0], connection[1]))
     
     return list(result)
 
 
 
   @property
-  def edges(self) -> List[Tuple[str, str]]:
-      """
-      Gets the edges of the graph.
+  def edges(self) -> List[Tuple[str, str, float]]:
+    """
+    Gets the edges of the graph.
 
-      Returns
-      -------
-      result : list of tuples (str, str)
+    Returns
+    -------
+    result : list of tuples (str, str, float)
 
-      """
-      return self.__get_edges()
+    """
+    return self.__get_edges()
 
 
 
-  def graph(self) -> List[Tuple[str, str, float]]:
-    return self.graph
+  @property
+  def graph(self) -> Dict[str, Set[Tuple[str, float]]]:
+    """
+    Gets the graph representation as an adjacency list.
+
+    Returns
+    -------
+    result : Dict[str, List[Set[str, float]]]
+
+    """
+    return self._graph
 
 
 
   @property
   def vertices(self) -> List[str]:
-      """
-      Gets the vertices of the graph.
+    """
+    Gets the vertices of the graph.
 
-      Returns
-      -------
-      result : list of str
+    Returns
+    -------
+    result : list of str
 
-      """
-      return list(self.graph.keys())
+    """
+    return list(self._graph.keys())
