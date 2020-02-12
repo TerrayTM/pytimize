1ef2b40e56e955cd44b44a2b285979694a7e60e7
Terry Zheng <terryzhengtx@gmail.com>
2020-02-12 17:23:24 -0500

    Implement type check utility

--- pytimize/utilities/_type_check.py
+++ pytimize/utilities/_type_check.py
@@ -1,5 +1,58 @@
-class TypeCheck:
-    @staticmethod
-    def expect(variable, types, parameter) -> None:
-        if not isinstance(variable, type_name):
-            raise ValueError(f"The parameter '{parameter}' needs to be of type '{type_name.__name__}'.")
+from typing import get_origin, get_args, _SpecialForm, Any
+from inspect import signature, _empty
+from functools import wraps
+from collections import deque
+
+def validate_type(input_value: Any, required_type: Any) -> bool:
+    queue = deque([(input_value, required_type)])
+
+    while len(queue) > 0:
+        value, expected_type = queue.pop()
+
+        if expected_type == _empty:
+            raise NotImplementedError("Type annotation is missing for parameter item.")
+        
+        if expected_type in [int, str, float, bool]:
+            if not isinstance(value, expected_type):
+                return expected_type == float and isinstance(value, int)
+        else:
+            origin = get_origin(expected_type)
+            
+            if isinstance(origin, _SpecialForm):
+                pass #TODO
+
+            if not isinstance(value, origin):
+                return False
+
+            argument_types = get_args(expected_type)
+
+            if origin == list or origin == set:
+                queue.extend((item, argument_types[0]) for item in value)
+            elif origin == tuple:
+                if not len(argument_types) == len(value):
+                    return False
+
+                queue.extend(zip(value, argument_types))
+            else:
+                raise NotImplementedError(f"Type check for `{origin}` is not implemented.")
+
+    return True
+
+
+
+def type_check(method: Any) -> Any:
+    @wraps(method)
+    def wrapped_method(*args, **kwargs) -> Any:
+        parameter_info = signature(method).parameters
+
+        kwargs.update(dict(zip(parameter_info.keys(), args)))
+
+        for variable, value in kwargs.items():
+            expected_type = parameter_info[variable].annotation
+
+            if not validate_type(value, expected_type):
+                raise TypeError(f"Parameter `{variable}` expects to have type `{expected_type}`.")
+        
+        return method(**kwargs)
+
+    return wrapped_method
