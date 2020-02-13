16144ad9101f004699153bb790933cd4d744d1cb
Terry Zheng <terryzhengtx@gmail.com>
2020-02-13 15:44:45 -0500

    Add support for union types

--- pytimize/utilities/_type_check.py
+++ pytimize/utilities/_type_check.py
@@ -1,4 +1,4 @@
-from typing import get_origin, get_args, _SpecialForm, Any
+from typing import get_origin, get_args, _SpecialForm, Any, Union
 from inspect import signature, _empty
 from functools import wraps
 from collections import deque
@@ -21,39 +21,75 @@ def _validate_type(input_value: Any, required_type: Any) -> bool:
         Whether or not the value is of the expected type.
 
     """
-    queue = deque([(input_value, required_type)])
+    queue = deque([(input_value, required_type, None)])
+    union_types = set()
+    union_id = 0
 
     while len(queue) > 0:
-        value, expected_type = queue.pop()
+        value, expected_type, ids = queue.pop()
 
         if expected_type == _empty:
             raise NotImplementedError("Type annotation is missing for parameter item.")
         
         if expected_type in [int, str, float, bool]:
             if not isinstance(value, expected_type):
-                return expected_type == float and isinstance(value, int)
+                if ids is None:
+                    return expected_type == float and isinstance(value, int)
+            elif ids is not None:
+                union_types.update(ids)
         else:
             origin = get_origin(expected_type)
-            
+
             if isinstance(origin, _SpecialForm):
-                pass #TODO
+                if origin == Union:
+                    base_copy = [union_id]
+
+                    if ids is not None:
+                        base_copy += ids.copy()
+
+                    queue.extend((value, allowed_type, base_copy.copy()) for allowed_type in get_args(expected_type))
+                    
+                    union_id +=1
+                    
+                    continue
+                else:
+                    raise NotImplementedError(f"Type check for special form `{origin}` is not supported.")
+
+            if origin is None: 
+                if ids is not None:
+                    union_types.update(ids)
+        
+                continue
 
             if not isinstance(value, origin):
-                return False
+                if ids is None:
+                    return False
+                else:
+                    continue
 
             argument_types = get_args(expected_type)
 
             if origin == list or origin == set:
-                queue.extend((item, argument_types[0]) for item in value)
+                queue.extend((item, argument_types[0], None if ids is None else ids.copy()) for item in value)
             elif origin == tuple:
-                if not len(argument_types) == len(value):
-                    return False
+                argument_count = len(argument_types)
 
-                queue.extend(zip(value, argument_types))
+                if argument_count == 0:
+                    raise NotImplementedError(f"Type annotation for collection type `{origin}` is incomplete.")
+
+                if not argument_count == len(value):
+                    if ids is None:
+                        return False
+                    else:
+                        continue
+
+                copy = [None if ids is None else ids.copy() for i in range(argument_count)]
+
+                queue.extend(zip(value, argument_types, copy))
             else:
                 raise NotImplementedError(f"Type check for `{origin}` is not implemented.")
 
-    return True
+    return all(i in union_types for i in range(union_id))
 
 
 
