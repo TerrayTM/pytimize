e27e74ca0ed12a9523e0284f16d4502fbcb44458
Terry Zheng <terryzhengtx@gmail.com>
2020-03-12 17:57:29 -0400

    Include negative variable and random order tests

--- pytimize/programs/tests/linear_program/test_to_sef.py
+++ pytimize/programs/tests/linear_program/test_to_sef.py
@@ -27,8 +27,10 @@ class TestToSEF(TestCase):
         self.assertTrue(np.allclose(p.b, np.array([5, 4, 2])), "Should compute correct constraint values in SEF.")
         self.assertTrue(np.allclose(p.c, np.array([-1, 2, -4, 4, 0, 0])), "Should compute correct coefficient vector in SEF.")
         self.assertTrue(math.isclose(p.z, 0), "Should compute correct constant in SEF.")
-        self.assertEqual(p.inequalities, ["=" for i in range(len(b))], "Should compute correct inequalities in SEF.")
+        self.assertEqual(p.inequalities, ["="] * len(b), "Should compute correct inequalities in SEF.")
         self.assertEqual(p.objective, "max", "Should be maximizing objective function in SEF.")
+        self.assertEqual(p.free_variables, [], "Should have no free variables.")
+        self.assertEqual(p.negative_variables, [], "Should have no negative variables.")
         
         A = np.array([
             [1, 2, 0, 1],
@@ -51,8 +53,90 @@ class TestToSEF(TestCase):
         self.assertTrue(np.allclose(p.b, np.array([10, 14, -2])), "Should compute correct constraint values in SEF.")
         self.assertTrue(np.allclose(p.c, np.array([-2, 3, -3, -4, 1, 0, 0])), "Should compute correct coefficient vector in SEF.")
         self.assertTrue(math.isclose(p.z, 0), "Should compute correct constant in SEF.")
-        self.assertEqual(p.inequalities, ["=" for i in range(len(b))], "Should compute correct inequalities in SEF.")
+        self.assertEqual(p.inequalities, ["="] * len(b), "Should compute correct inequalities in SEF.")
         self.assertEqual(p.objective, "max", "Should be maximizing objective function in SEF.")
+        self.assertEqual(p.free_variables, [], "Should have no free variables.")
+        self.assertEqual(p.negative_variables, [], "Should have no negative variables.")
+
+        A = np.array([
+            [1, 2, 0, 1],
+            [1, -2, 16, 0],
+            [8, 2, -3, 1]
+        ])
+        b = np.array([10, 14, -2])
+        c = np.array([-2, 3, -4, 1])
+        z = 0
+
+        p = LinearProgram(A, b, c, z, "max", ["<=", "<=", "="], [2], [3, 4])
+        
+        p.to_sef(in_place=True)
+
+        self.assertTrue(np.allclose(p.A, np.array([
+            [1, 2, -2, 0, -1, 1, 0],
+            [1, -2, 2, -16, 0, 0, 1],
+            [8, 2, -2, 3, -1, 0, 0]
+        ])), "Should compute correct coefficient matrix in SEF.")
+        self.assertTrue(np.allclose(p.b, np.array([10, 14, -2])), "Should compute correct constraint values in SEF.")
+        self.assertTrue(np.allclose(p.c, np.array([-2, 3, -3, 4, -1, 0, 0])), "Should compute correct coefficient vector in SEF.")
+        self.assertTrue(math.isclose(p.z, 0), "Should compute correct constant in SEF.")
+        self.assertEqual(p.inequalities, ["="] * len(b), "Should compute correct inequalities in SEF.")
+        self.assertEqual(p.objective, "max", "Should be maximizing objective function in SEF.")
+        self.assertEqual(p.free_variables, [], "Should have no free variables.")
+        self.assertEqual(p.negative_variables, [], "Should have no negative variables.")
+
+    def test_random_order_free_variables(self):
+        A = np.array([
+            [1, 2, 0, 1],
+            [1, -2, 16, 0],
+            [8, 2, -3, 1]
+        ])
+        b = np.array([10, 14, -2])
+        c = np.array([-2, 3, -4, 1])
+        z = 0
+
+        p = LinearProgram(A, b, c, z, "max", free_variables=[2, 1])
+        
+        p.to_sef(in_place=True)
+
+        self.assertTrue(np.allclose(p.A, np.array([
+            [1, -1, 2, -2, 0, 1],
+            [1, -1, -2, 2, 16, 0],
+            [8, -8, 2, -2, -3, 1]
+        ])), "Should compute correct coefficient matrix in SEF.")
+        self.assertTrue(np.allclose(p.b, np.array([10, 14, -2])), "Should compute correct constraint values in SEF.")
+        self.assertTrue(np.allclose(p.c, np.array([-2, 2, 3, -3, -4, 1])), "Should compute correct coefficient vector in SEF.")
+        self.assertTrue(math.isclose(p.z, 0), "Should compute correct constant in SEF.")
+        self.assertEqual(p.inequalities, ["="] * len(b), "Should compute correct inequalities in SEF.")
+        self.assertEqual(p.objective, "max", "Should be maximizing objective function in SEF.")
+        self.assertEqual(p.free_variables, [], "Should have no free variables.")
+        self.assertEqual(p.negative_variables, [], "Should have no negative variables.")
+
+    def test_random_order_negative_variables(self):
+        A = np.array([
+            [0, 0, 1],
+            [1, 0, 0],
+            [0, 1, 0]
+        ])
+        b = np.array([10, 14, -2])
+        c = np.array([2, 3, 4])
+        z = 0
+
+        p = LinearProgram(A, b, c, z, "max", free_variables=[2], negative_variables=[3, 1])
+        
+        p.to_sef(in_place=True)
+
+        self.assertTrue(np.allclose(p.A, np.array([
+            [0, 0, 0, -1],
+            [-1, 0, 0, 0],
+            [0, 1, -1, 0]
+        ])), "Should compute correct coefficient matrix in SEF.")
+        self.assertTrue(np.allclose(p.b, np.array([10, 14, -2])), "Should compute correct constraint values in SEF.")
+        self.assertTrue(np.allclose(p.c, np.array([-2, 3, -3, -4])), "Should compute correct coefficient vector in SEF.")
+        self.assertTrue(math.isclose(p.z, 0), "Should compute correct constant in SEF.")
+        self.assertEqual(p.inequalities, ["="] * len(b), "Should compute correct inequalities in SEF.")
+        self.assertEqual(p.objective, "max", "Should be maximizing objective function in SEF.")
+        self.assertEqual(p.free_variables, [], "Should have no free variables.")
+        self.assertEqual(p.negative_variables, [], "Should have no negative variables.")
 
 if __name__ == "__main__":
     main()
