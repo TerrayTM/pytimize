a629dbf2f018fc434766cbf729e737f27e85b544
Terry Zheng <terryzhengtx@gmail.com>
2020-03-31 16:04:15 -0700

    Update setup properties

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as file:
 setup(
     name="pytimize",
     packages=["pytimize"],
-    version="0.0.1a",
+    version="0.0.1a1",
     description="Python optimization library for mathematical programming.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -16,8 +16,15 @@ setup(
     maintainer_email="pytimize@terrytm.com",
     url="https://pytimize.terrytm.com",
     python_requires=">=3.8",
+    keywords="mathematical programming optimization",
+    license="Apache 2.0",
     zip_safe=False,
     install_requires=["numpy", "matplotlib"],
+    project_urls={
+        "Bug Reports": "https://pytimize.terrytm.com/issues",
+        "Documentation": "https://pytimize.terrytm.com",
+        "Source Code": "https://github.com/TerrayTM/pytimize"
+    },
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Science/Research",
