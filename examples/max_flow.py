a2c908a6eb6073610efb4f337684f68f31ce47e0
Terry Zheng <terryzhengtx@gmail.com>
2021-01-16 16:34:53 -0800

    Add max flow example

---
+++ examples/max_flow.py
@@ -0,0 +1,26 @@
+from pytimize.graphs import DirectedGraph
+
+# Define directed graph with arcs and their capcities
+g = DirectedGraph(arcs={
+    ("s", "c"): 15,
+    ("a", "b"): 3,
+    ("a", "d"): 4,
+    ("c", "b"): 4,
+    ("c", "d"): 6,
+    ("b", "t"): 10,
+    ("d", "t"): 5
+})
+
+# Compute max flow using Preflow Push algorithm
+value, flow = g.preflow_push("s", "t")
+
+print("\nPreflow Push ===========================================")
+print(f"Max Flow: {value}")
+print(f"Flow: {flow}")
+
+# Compute max flow using Ford-Fulkerson algorithm
+value, flow = g.ford_fulkerson("s", "t")
+
+print("\nFord Fulkerson =========================================")
+print(f"Max Flow: {value}")
+print(f"Flow: {flow}")
